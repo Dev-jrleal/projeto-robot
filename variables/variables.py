43993d620823d8082048cc16b9eea28b0a4814af
junior-leal <dev.junior.leal@gmail.com>
2024-12-21 20:12:57 -0300

    We refactored the automation so that the variables are external to the .robot and .resource files

---
+++ variables/variables.py
@@ -0,0 +1,17 @@
+LOGIN_DATA = {
+    "correctData":{
+        "username":"standard_user",
+        "password":"secret_sauce"
+    },
+    "wrongData":{
+        "username":"junior",
+        "password":"123456"
+    }
+}
+
+ERROR_MESSAGES = {
+    "noUserName":"Epic sadface: Username is required",
+    "noCredentials":"Epic sadface: Username is required",
+    "noPassword":"Epic sadface: Password is required",
+    "wrongCredentials":"Epic sadface: Username and password do not match any user in this service"
+}
